0818a7a57f276443a5ea36b9555dcda86bc75cc7
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-22 16:38:17 -0500

    Se generan los controladores para las demas rutas del proyecto.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UsersController } from './controllers/users/users.controller';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { OrdersController } from './controllers/orders/orders.controller';
+import { ProductsController } from './controllers/products/products.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
+
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    UsersController,
+    OrdersController,
+    CustomersController,
+    BrandsController,
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
